4405247450ed88484c9dcc1e770ef1f7413f3a4f
AngularMark <AngularMark.github.com@ma7.org>
2014-01-01 16:54:17 -0600

    Initialize repository

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('2014LearnAngularDirApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
